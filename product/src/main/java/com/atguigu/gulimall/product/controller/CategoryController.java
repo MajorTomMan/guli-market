e14e27df69332ff0f41b605a03febbb36d997cc7
flashnames <765719516@qq.com>
2022-07-31 23:58:32 +0800

    增加商品api

--- product/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
+++ product/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.atguigu.gulimall.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 // import org.apache.shiro.authz.annotation.RequiresPermissions;
@@ -40,6 +41,14 @@ public class CategoryController {
         PageUtils page = categoryService.queryPage(params);
 
         return R.ok().put("page", page);
+    }
+
+    @RequestMapping("/list/tree")
+    // @RequiresPermissions("product:category:list")
+    public R list(){
+        List<CategoryEntity> entities = categoryService.listWithTree();
+
+        return R.ok().put("data", entities);
     }
 
 
--- product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,14 @@
 package com.atguigu.gulimall.product.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.Locale.Category;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -12,18 +19,23 @@ import com.atguigu.gulimall.product.dao.CategoryDao;
 import com.atguigu.gulimall.product.entity.CategoryEntity;
 import com.atguigu.gulimall.product.service.CategoryService;
 
-
 @Service("categoryService")
 public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService {
+    @Autowired
+    CategoryDao categoryDao;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<CategoryEntity> page = this.page(
                 new Query<CategoryEntity>().getPage(params),
-                new QueryWrapper<CategoryEntity>()
-        );
+                new QueryWrapper<CategoryEntity>());
 
         return new PageUtils(page);
     }
 
+    public List<CategoryEntity> listWithTree() {
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+        List<CategoryEntity> topLevelMenus = entities.stream().filter(categoryEntity-> categoryEntity.getParentCid() == 0).collect(Collectors.toList());
+        return topLevelMenus;
+    }
 }
