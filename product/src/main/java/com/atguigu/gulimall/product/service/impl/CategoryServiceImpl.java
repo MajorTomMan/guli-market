ee5b97d2c363d96637cccd2e6a7f809657a67408
flashnames <765719516@qq.com>
2022-08-12 20:22:17 +0800

    增加跨域请求和三级菜单代码

--- product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -35,7 +35,35 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
     public List<CategoryEntity> listWithTree() {
         List<CategoryEntity> entities = baseMapper.selectList(null);
-        List<CategoryEntity> topLevelMenus = entities.stream().filter(categoryEntity-> categoryEntity.getParentCid() == 0).collect(Collectors.toList());
+        List<CategoryEntity> topLevelMenus = entities.stream().filter(
+                categoryEntity-> categoryEntity.getParentCid() == 0
+            ).map(
+            (menu)->{
+                menu.setChildren(getChildrens(menu, entities));
+                return menu;
+            }
+        ).sorted(
+            (menu1,menu2)->{
+                return menu1.getSort()-menu2.getSort();
+            }
+        ).collect(Collectors.toList());
         return topLevelMenus;
+    }
+    private List<CategoryEntity> getChildrens(CategoryEntity root,List<CategoryEntity> all){
+        List<CategoryEntity> children = all.stream().filter(
+            categoryEntity->{
+                return categoryEntity.getParentCid()==root.getCatId();
+            }
+        ).map(
+            categoryEntity->{
+                categoryEntity.setChildren(getChildrens(categoryEntity, all));
+                return categoryEntity;
+            }
+        ).sorted(
+            (menu1,menu2)->{
+                return (menu1.getSort()==null?0:menu1.getSort())-(menu2.getSort()==null?0:menu2.getSort());
+            }
+        ).collect(Collectors.toList());
+        return children;
     }
 }
