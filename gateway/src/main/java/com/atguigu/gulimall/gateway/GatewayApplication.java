58a8c3d138a8742668fe085bd652b11149fc0e4f
flashnames <765719516@qq.com>
2022-08-16 23:50:14 +0800

    增加树形菜单的显示代码

--- gateway/src/main/java/com/atguigu/gulimall/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/atguigu/gulimall/gateway/GatewayApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class GatewayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
