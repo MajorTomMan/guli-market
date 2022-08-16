ca85031c1fad9e9ad3e8785e5d703a5c10e96fec
flashnames <765719516@qq.com>
2022-08-16 20:21:24 +0800

    修复了网关路由至renren-fast项目时出现的503问题和浏览器跨域问题

--- gateway/src/main/java/com/atguigu/gulimall/gateway/config/gatewayCorsConfiguration.java
+++ gateway/src/main/java/com/atguigu/gulimall/gateway/config/gatewayCorsConfiguration.java
@@ -1,10 +1,12 @@
 package com.atguigu.gulimall.gateway.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
+@Configuration
 public class gatewayCorsConfiguration {
     @Bean
     public CorsWebFilter corsWebFilter() {
