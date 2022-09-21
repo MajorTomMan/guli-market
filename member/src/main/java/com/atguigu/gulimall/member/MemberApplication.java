d1d8a46bae689fc65fc39f875e8ca030c5117beb
flashnames <765719516@qq.com>
2022-09-21 10:11:13 +0800

    修改公共依赖里的依赖包,以及会员服务的配置

--- member/src/main/java/com/atguigu/gulimall/member/MemberApplication.java
+++ member/src/main/java/com/atguigu/gulimall/member/MemberApplication.java
@@ -1,3 +1,11 @@
+/*
+ * @Author: flashnames 765719516@qq.com
+ * @Date: 2022-07-21 16:08:04
+ * @LastEditors: flashnames 765719516@qq.com
+ * @LastEditTime: 2022-09-21 09:07:29
+ * @FilePath: /common/home/master/project/gulimall/member/src/main/java/com/atguigu/gulimall/member/MemberApplication.java
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 package com.atguigu.gulimall.member;
 
 import org.springframework.boot.SpringApplication;
@@ -5,14 +13,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-
 @EnableFeignClients(basePackages = "com.atguigu.gulimall.member.feign")
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MemberApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MemberApplication.class, args);
     }
-
 }
