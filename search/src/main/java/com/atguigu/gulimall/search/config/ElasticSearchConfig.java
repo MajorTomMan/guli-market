6a720d0f1b52886b222a8140fe0d2661756ff536
flashnames <765719516@qq.com>
2023-01-31 23:56:03 +0800

    根据官方文档修改了测试方法和代码

--- search/src/main/java/com/atguigu/gulimall/search/config/ElasticSearchConfig.java
+++ search/src/main/java/com/atguigu/gulimall/search/config/ElasticSearchConfig.java
@@ -2,24 +2,49 @@
  * @Author: flashnames 765719516@qq.com
  * @Date: 2023-01-30 13:20:29
  * @LastEditors: flashnames 765719516@qq.com
- * @LastEditTime: 2023-01-30 13:25:46
- * @FilePath: /GuliMall/search/src/main/java/com/atguigu/gulimall/search/config/ElasticSearchConfig.java
+ * @LastEditTime: 2023-01-31 23:36:56
+ * @FilePath: /common/home/master/project/GuliMall/search/src/main/java/com/atguigu/gulimall/search/config/ElasticSearchConfig.java
  * @Description: 
  * 
  * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
  */
 package com.atguigu.gulimall.search.config;
 
+import org.apache.http.Header;
 import org.apache.http.HttpHost;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.message.BasicHeader;
+import org.elasticsearch.client.Node;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClient.FailureListener;
 import org.elasticsearch.client.RestClientBuilder;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ElasticSearchConfig {
-    public RestClient restClient(){
-        HttpHost host = new HttpHost("localhost",9200,"http");
-        RestClientBuilder builder=RestClient.builder(host);
+    public RestClient restClient() {
+        HttpHost host = new HttpHost("localhost", 9200, "http");
+        RestClientBuilder builder = RestClient.builder(host);
+        /* 设置头部 */
+        Header[] defaultHeader = new Header[] { new BasicHeader("Content-Type", "application/json") };
+        builder.setDefaultHeaders(defaultHeader);
+        /* 设置错误监听函数 */
+        builder.setFailureListener(
+                new FailureListener() {
+                    @Override
+                    public void onFailure(Node node) {
+                        System.out.println(node);
+                    }
+                });
+        /* 设置默认请求配置回调 */
+        builder.setRequestConfigCallback(
+                new RestClientBuilder.RequestConfigCallback() {
+                    @Override
+                    public RequestConfig.Builder customizeRequestConfig(
+                            RequestConfig.Builder requestConfigBuilder) {
+                        return requestConfigBuilder.setSocketTimeout(10000);
+                    }
+                });
         return builder.build();
     }
 }
--- search/src/test/java/com/atguigu/gulimall/search/SearchApplicationTests.java
+++ search/src/test/java/com/atguigu/gulimall/search/SearchApplicationTests.java
@@ -2,7 +2,7 @@
  * @Author: flashnames 765719516@qq.com
  * @Date: 2023-01-30 13:16:00
  * @LastEditors: flashnames 765719516@qq.com
- * @LastEditTime: 2023-01-30 18:43:01
+ * @LastEditTime: 2023-01-31 23:51:21
  * @FilePath: /common/home/master/project/GuliMall/search/src/test/java/com/atguigu/gulimall/search/SearchApplicationTests.java
  * @Description: 
  * 
@@ -11,18 +11,24 @@
 package com.atguigu.gulimall.search;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import com.alibaba.nacos.common.utils.JacksonUtils;
 import com.atguigu.gulimall.search.enitty.TestEntity;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.ParseException;
 import org.apache.http.entity.ContentType;
 import org.apache.http.nio.entity.NStringEntity;
 import org.apache.http.util.EntityUtils;
+import org.elasticsearch.client.Cancellable;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
+import org.elasticsearch.client.ResponseListener;
 import org.elasticsearch.client.RestClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,8 +44,11 @@ class SearchApplicationTests {
         String type="_doc";
         String id="1";
         String path=createPath(index, type, id);
-        String json = new ObjectMapper().writeValueAsString(new TestEntity("gg", "man"));
-        SendData("post",json, path);
+        Map<String,List<TestEntity>> data=new HashMap<>();
+        List<TestEntity> list=new ArrayList<>();
+        list.add(new TestEntity("gg", "gg"));
+        data.put("doc", list);
+        SendData("post",path,data);
     }
     @Test
     public void deleteData() throws IOException{
@@ -47,7 +56,7 @@ class SearchApplicationTests {
         String type="_doc";
         String id="1";
         String path=createPath(index, type, id);
-        SendData("delete", path);
+        SendData("delete", path,Collections.emptyMap());
     }
     @Test
     public void queryData() throws IOException{
@@ -55,7 +64,7 @@ class SearchApplicationTests {
         String type="_doc";
         String id="1";
         String path=createPath(index, type, id);
-        SendData("get", path);
+        SendData("get", path,Collections.emptyMap());
     }
     @Test
     public void updateData() throws IOException{
@@ -63,35 +72,42 @@ class SearchApplicationTests {
         String type="_update";
         String id="1";
         String path=createPath(index, type, id);
-        Map<String,TestEntity> data=new HashMap<>();
-        data.put("doc", new TestEntity("GGGGGGGG","gggggg"));
-        String json = new ObjectMapper().writeValueAsString(data);
-        System.out.println(json);
-        SendData("post",json, path);
+        Map<String,List<TestEntity>> data=new HashMap<>();
+        List<TestEntity> list=new ArrayList<>();
+        list.add(new TestEntity("gg", "gg"));
+        data.put("doc",list);
+        SendData("post",path,data);
     }
-    private void SendData(String method, String path) throws IOException {
-        Request request=new Request(method,path);
-        Response response = restClient.performRequest(request);
-        HttpEntity entity = response.getEntity();
-        String data = EntityUtils.toString(entity, "UTF8");
-        System.out.println("data:"+data);
-        System.out.println(entity.getContentType());
-        System.out.println("status:"+response.getStatusLine().getStatusCode());
-        System.out.println("hostname:"+response.getHost().getHostName());
-    }
-    private void SendData(String method,String data,String path) throws IOException{
-        Request request=new Request(method,path);
-        HttpEntity entity=new NStringEntity(data,ContentType.APPLICATION_JSON);
-        request.setEntity(entity);
-        Response response = restClient.performRequest(request);
-        String jsondata = EntityUtils.toString(entity, "UTF8");
-        System.out.println("data:"+jsondata);
-        System.out.println(entity.getContentType());
-        System.out.println("status:"+response.getStatusLine().getStatusCode());
-        System.out.println("hostname:"+response.getHost().getHostName());
+    private void SendData(String method, String path,Map<String,List<TestEntity>> entities) throws IOException {
+        Request request=new Request(method, path);
+        if(!entities.isEmpty()){
+            String json = JacksonUtils.toJson(entities);
+            HttpEntity entity=new NStringEntity(json,ContentType.APPLICATION_JSON);
+            request.setEntity(entity);
+        }
+        Cancellable cancellable = restClient.performRequestAsync(request, new ResponseListener() {
+            @Override
+            public void onSuccess(Response response) {
+                // TODO Auto-generated method stub
+                HttpEntity entity=response.getEntity();
+                try {
+                    String data = EntityUtils.toString(entity);
+                    System.out.println(data);
+                } catch (ParseException | IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(Exception exception) {
+                // TODO Auto-generated method stub
+                System.out.println(exception.getMessage());
+                exception.printStackTrace();
+            }
+        });
+        cancellable.cancel();
     }
     private String createPath(String index,String type,String id){
         return "/"+index+"/"+type+"/"+id;
-        
     }
 }
