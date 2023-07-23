8bbed90fe540642fb35e134f03b3c9386cdd592d
MajorTomMan <765719516@qq.com>
2023-07-23 23:54:40 +0800

    将多余的子依赖去掉,精简pom文件

--- member/src/main/java/com/atguigu/gulimall/member/MemberApplication.java
+++ member/src/main/java/com/atguigu/gulimall/member/MemberApplication.java
@@ -2,7 +2,7 @@
  * @Author: flashnames 765719516@qq.com
  * @Date: 2022-07-21 16:08:04
  * @LastEditors: MajorTomMan 765719516@qq.com
- * @LastEditTime: 2023-07-19 22:58:31
+ * @LastEditTime: 2023-07-23 23:36:03
  * @FilePath: /common/home/master/project/gulimall/member/src/main/java/com/atguigu/gulimall/member/MemberApplication.java
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,7 +11,6 @@ package com.atguigu.gulimall.member;
 import org.redisson.spring.starter.RedissonAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
