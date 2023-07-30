1289bdf0c1c5b629b8196bad21c8e031d1d70dbe
MajorTomMan <765719516@qq.com>
2023-07-31 00:00:50 +0800

    按照ES8的标准修改

--- search/src/main/java/com/atguigu/gulimall/search/service/impl/SearchServiceImpl.java
+++ search/src/main/java/com/atguigu/gulimall/search/service/impl/SearchServiceImpl.java
@@ -2,13 +2,15 @@
  * @Author: MajorTomMan 765719516@qq.com
  * @Date: 2023-07-24 23:32:03
  * @LastEditors: MajorTomMan 765719516@qq.com
- * @LastEditTime: 2023-07-30 23:03:28
+ * @LastEditTime: 2023-07-31 00:00:23
  * @FilePath: \Guli\search\src\main\java\com\atguigu\gulimall\search\service\impl\SearchServiceImpl.java
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 package com.atguigu.gulimall.search.service.impl;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +20,13 @@ import org.springframework.util.StringUtils;
 import com.atguigu.gulimall.search.service.SearchService;
 import com.atguigu.gulimall.search.vo.SearchParam;
 import com.atguigu.gulimall.search.vo.SearchResult;
+import com.esotericsoftware.minlog.Log;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch._types.ElasticsearchException;
 import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
 import co.elastic.clients.elasticsearch._types.query_dsl.NestedQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
 import co.elastic.clients.elasticsearch._types.query_dsl.QueryBuilders;
 import co.elastic.clients.elasticsearch._types.query_dsl.RangeQuery;
 import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery.Builder;
@@ -61,57 +65,67 @@ public class SearchServiceImpl implements SearchService {
 
     /* 准备检索请求 */
     private SearchResult buildSearchRequest(SearchParam param) throws ElasticsearchException, IOException {
-        Builder bool = QueryBuilders.bool();
+        List<Query> querys = new ArrayList<>();
         /* 模糊匹配 */
         if (!StringUtils.hasText(param.getKeyword())) {
-            bool.must(m -> m.match(match -> match.field("skuTitle").query(param.getKeyword())));
+            Query query = BoolQuery
+                    .of(b -> b.must(m -> m.match(match -> match.field("skuTitle").query(param.getKeyword()))))
+                    ._toQuery();
+            querys.add(query);
         }
         /* 按照三级分类查询 */
         if (!ObjectUtils.isEmpty(param.getCatalog3Id())) {
-            bool.filter(f -> f.term(t -> t.field("catalogId").value(param.getCatalog3Id())));
+            Query query = BoolQuery
+                    .of(b -> b.filter(f -> f.term(t -> t.field("catalogId").value(param.getCatalog3Id()))))._toQuery();
+            querys.add(query);
         }
         /* 按照品牌ID查询 */
         if (!ObjectUtils.isEmpty(param.getBrandId())) {
-            bool.filter(f -> f.terms(t -> t.field("brandId")));
+            Query query = BoolQuery.of(b -> b.filter(f -> f.terms(t -> t.field("brandId"))))._toQuery();
+            querys.add(query);
         }
         /* 按照指定的属性进行查询 */
+        /* 尚未完成 */
         if (param.getAttrs() != null && param.getAttrs().size() > 0) {
-            BoolQuery.Builder nestedQuery=QueryBuilders.bool();
+            BoolQuery.Builder nestedQuery = QueryBuilders.bool();
             for (String attr : param.getAttrs()) {
-                String[] temp=attr.split("_");
-                String attrId=temp[0];
-                String[] attrValues=temp[1].split(":");
-                nestedQuery.must(m->m.term(t->t.field("attrs.attrId").value(attrId)));
+                String[] temp = attr.split("_");
+                String attrId = temp[0];
+                String[] attrValues = temp[1].split(":");
+                nestedQuery.must(m -> m.term(t -> t.field("attrs.attrId").value(attrId)));
             }
 
             NestedQuery.Builder query = QueryBuilders.nested().path("attrs");
         }
-
+        BoolQuery boolQuery = null;
         /* 按照是否有库存进行查询 */
         if (param.getHasStock() == 1) {
-            bool.filter(f -> f.term(t -> t.field("hasStock").value(true)));
+            boolQuery = BoolQuery.of(b -> b.filter(f -> f.term(t -> t.field("hasStock").value(true))));
         } else {
-            bool.filter(f -> f.term(t -> t.field("hasStock").value(false)));
+            boolQuery = BoolQuery.of(b -> b.filter(f -> f.term(t -> t.field("hasStock").value(false))));
         }
+        querys.add(boolQuery._toQuery());
         /* 按照价格区间来查询 */
         if (!StringUtils.hasText(param.getSkuPrice())) {
             /*
              * 三种情况
              * 1_500/_500/500_
              */
-            RangeQuery.Builder query = QueryBuilders.range().field("skuPrice");
             String[] temp = param.getSkuPrice().split("_");
+            RangeQuery rangeQuery = null;
             if (temp.length == 2) {
-                query.gte(JsonData.of(temp[0], new SimpleJsonpMapper()))
-                        .lte(JsonData.of(temp[1], new SimpleJsonpMapper()));
+                rangeQuery = RangeQuery.of(r -> r.field("skuPrice").gte(JsonData.of(temp[0]))
+                        .lte(JsonData.of(temp[1])));
             } else if (temp.length == 1) {
                 if (param.getSkuPrice().startsWith("_")) {
-                    query.lte(JsonData.of(temp[0], new SimpleJsonpMapper()));
+                    rangeQuery = RangeQuery.of(r -> r.field("skuPrice").lte(JsonData.of(temp[1])));
                 } else if (param.getSkuPrice().endsWith("_")) {
-                    query.gte(JsonData.of(temp[0], new SimpleJsonpMapper()));
+                    rangeQuery = RangeQuery.of(r -> r.field("skuPrice").gte(JsonData.of(temp[1])));
                 }
             }
-            bool.filter(f -> f.range(query.build()));
+            final RangeQuery range = rangeQuery;
+            Query query = BoolQuery.of(b -> b.filter(f -> f.range(range)))._toQuery();
+            querys.add(query);
         }
         return null;
     }
