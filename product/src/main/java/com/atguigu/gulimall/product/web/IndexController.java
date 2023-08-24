df61583cf12bf3e3359c264ee63277d91f305635
MajorTomMan <765719516@qq.com>
2023-08-25 00:10:49 +0800

    修改错误的路由规则

--- product/src/main/java/com/atguigu/gulimall/product/web/IndexController.java
+++ product/src/main/java/com/atguigu/gulimall/product/web/IndexController.java
@@ -1,8 +1,8 @@
 /*
  * @Author: flashnames 765719516@qq.com
  * @Date: 2023-02-13 23:22:09
- * @LastEditors: flashnames 765719516@qq.com
- * @LastEditTime: 2023-02-26 10:40:12
+ * @LastEditors: MajorTomMan 765719516@qq.com
+ * @LastEditTime: 2023-08-25 00:08:23
  * @FilePath: /common/home/master/project/GuliMall/product/src/main/java/com/atguigu/gulimall/product/web/IndexController.java
  * @Description: 
  * 
@@ -42,7 +42,7 @@ public class IndexController {
     /* 不要和Nginx中的静态匹配路径一样,不然nginx会默认你访问的是静态资源而报404 */
     /* 不要写/static/ */
     @ResponseBody
-    @GetMapping("/index/catalog.json")
+    @GetMapping("index/catalog.json")
     public Map<String, List<Catelog2Vo>> getCatalogJson() throws JsonProcessingException, InterruptedException {
         Map<String, List<Catelog2Vo>> catalogJson = categoryService.getCatalogJson();
         return catalogJson;
