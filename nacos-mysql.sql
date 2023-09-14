df7a31667501efccadb1da229662b4c47bf5e4b4
MajorTomMan <765719516@qq.com>
2023-09-14 22:29:59 +0800

    修改Nacos的Mysql文件以和当前Nacos对应,将Nacos从内嵌数据库转为Mysql

--- nacos-mysql.sql
+++ nacos-mysql.sql
@@ -1,4 +1,3 @@
--- Active: 1688392670869@@192.168.253.131@3306@renren_security
 /*
  * Copyright 1999-2018 Alibaba Group Holding Ltd.
  *
@@ -15,19 +14,17 @@
  * limitations under the License.
  */
 
+drop database if exists nacos_config;
+create database if not exists nacos_config;
+use nacos_config;
 /******************************************/
 /*   数据库全名 = nacos_config   */
 /*   表名称 = config_info   */
 /******************************************/
-
-drop database if EXISTS nacos_config;
-create database if NOT EXISTS nacos_config default CHARACTER set utf8mb4;
-use nacos_config;
-
 CREATE TABLE `config_info` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
   `data_id` varchar(255) NOT NULL COMMENT 'data_id',
-  `group_id` varchar(255) DEFAULT NULL,
+  `group_id` varchar(128) DEFAULT NULL,
   `content` longtext NOT NULL COMMENT 'content',
   `md5` varchar(32) DEFAULT NULL COMMENT 'md5',
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
@@ -41,6 +38,7 @@ CREATE TABLE `config_info` (
   `effect` varchar(64) DEFAULT NULL,
   `type` varchar(64) DEFAULT NULL,
   `c_schema` text,
+  `encrypted_data_key` text NOT NULL COMMENT '秘钥',
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_configinfo_datagrouptenant` (`data_id`,`group_id`,`tenant_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='config_info';
@@ -52,7 +50,7 @@ CREATE TABLE `config_info` (
 CREATE TABLE `config_info_aggr` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
   `data_id` varchar(255) NOT NULL COMMENT 'data_id',
-  `group_id` varchar(255) NOT NULL COMMENT 'group_id',
+  `group_id` varchar(128) NOT NULL COMMENT 'group_id',
   `datum_id` varchar(255) NOT NULL COMMENT 'datum_id',
   `content` longtext NOT NULL COMMENT '内容',
   `gmt_modified` datetime NOT NULL COMMENT '修改时间',
@@ -80,6 +78,7 @@ CREATE TABLE `config_info_beta` (
   `src_user` text COMMENT 'source user',
   `src_ip` varchar(50) DEFAULT NULL COMMENT 'source ip',
   `tenant_id` varchar(128) DEFAULT '' COMMENT '租户字段',
+  `encrypted_data_key` text NOT NULL COMMENT '秘钥',
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_configinfobeta_datagrouptenant` (`data_id`,`group_id`,`tenant_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='config_info_beta';
@@ -146,7 +145,7 @@ CREATE TABLE `group_capacity` (
 /*   表名称 = his_config_info   */
 /******************************************/
 CREATE TABLE `his_config_info` (
-  `id` bigint(64) unsigned NOT NULL,
+  `id` bigint(20) unsigned NOT NULL,
   `nid` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `data_id` varchar(255) NOT NULL,
   `group_id` varchar(128) NOT NULL,
@@ -159,6 +158,7 @@ CREATE TABLE `his_config_info` (
   `src_ip` varchar(50) DEFAULT NULL,
   `op_type` char(10) DEFAULT NULL,
   `tenant_id` varchar(128) DEFAULT '' COMMENT '租户字段',
+  `encrypted_data_key` text NOT NULL COMMENT '秘钥',
   PRIMARY KEY (`nid`),
   KEY `idx_gmt_create` (`gmt_create`),
   KEY `idx_gmt_modified` (`gmt_modified`),
