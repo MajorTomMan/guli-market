4fea3a17a0ba9b58a98d32adfaafc03104ad57ca
MajorTomMan <765719516@qq.com>
2023-09-17 16:36:46 +0800

    修改正确的ID

--- order/src/main/java/com/atguigu/gulimall/order/entity/MqMessageEntity.java
+++ order/src/main/java/com/atguigu/gulimall/order/entity/MqMessageEntity.java
@@ -1,3 +1,11 @@
+/*
+ * @Author: MajorTomMan 765719516@qq.com
+ * @Date: 2023-06-23 17:37:38
+ * @LastEditors: MajorTomMan 765719516@qq.com
+ * @LastEditTime: 2023-09-17 16:31:33
+ * @FilePath: \Guli\order\src\main\java\com\atguigu\gulimall\order\entity\MqMessageEntity.java
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 package com.atguigu.gulimall.order.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
@@ -23,7 +31,7 @@ public class MqMessageEntity implements Serializable {
 	/**
 	 * 
 	 */
-	@TableId(value = "id",type = IdType.AUTO)
+	@TableId(value = "message_id",type = IdType.AUTO)
 	private String messageId;
 	/**
 	 * 
