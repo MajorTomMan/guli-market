23ba586a01f4690fd259ace35c9c1e0926c745f2
MajorTomMan <765719516@qq.com>
2024-08-08 23:20:22 +0800

    完成首页显示登录名字

--- product/src/main/java/com/atguigu/gulimall/product/config/SessionConfig.java
+++ product/src/main/java/com/atguigu/gulimall/product/config/SessionConfig.java
@@ -1,17 +1,22 @@
-package com.atguigu.gulimall.common.config;
+/*
+ * @Date: 2024-08-08 18:12:17
+ * @LastEditors: MajorTomMan 765719516@qq.com
+ * @LastEditTime: 2024-08-08 23:17:33
+ * @FilePath: \Guli\product\src\main\java\com\atguigu\gulimall\product\config\SessionConfig.java
+ * @Description: MajorTomMan @版权声明 保留文件所有权利
+ */
+package com.atguigu.gulimall.product.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
-@EnableRedisHttpSession
 public class SessionConfig {
     
     @Bean
